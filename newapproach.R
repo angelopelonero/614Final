dfa6c580628946d2274c26205ca52ea8f6852a1e
Angelo Pelonero <angelo@angelos-mbp.gateway.sonic.net>
2018-05-01 23:19:09 -0700

    Data mostly pre-processed, nearly ready for model application.

--- newapproach.R
+++ newapproach.R
@@ -1,5 +1,10 @@
 ### read in data, gets rid of NAs (in this data, an NA is noted by "?")
 setwd("~/Documents/USF/HS614/DiabData")
+library(caret)
+library(DataCombine)
+library(onehot)
+library(devtools); library(ggfortify); library(ggplot2)
+library(randomForest)
 
 data1 <- read.csv("diabetic_data.csv", na.strings = "?")
 data2 <- data1
@@ -16,7 +21,7 @@ colnames(data2)
 data2 <- data2[ ,-nearZeroVar(data2)]
 colnames(data2)
 
-library(DataCombine)
+
 data2$weight <-NULL
 data2$payer_code <- NULL
 data2$medical_specialty <- NULL
@@ -30,28 +35,36 @@ qual_data <- dplyr::select_if(data2, is.factor)
 
 ### Clean categorical data here, then run random forest to figure out which features are most important
 
-library(randomForest)
+### Onehot encoding of nominal vars - I don't quite get this yet
+
+#qual_data <- onehot(qual_data)
+
+### Random forest feature selection here:
+
 forestation <- randomForest(formula = readmitted ~ ., data = qual_data, importance = TRUE)
 
 ### Use PCA to generate features to merge with those qual features selected via random forests 
 
 prin_comp <- prcomp(na.omit(quant_data), center = TRUE, scale = TRUE)
 names(prin_comp)
-
-library(devtools); library(ggfortify); library(ggplot2)
 biplot(prin_comp)
 
 ### This plot shows which principal components are actually worth their salt
+
 plot(prin_comp)
 
 ### The first two components seem to account for most of the data's variability
+### Therefore we selet the first two PCs wheb binding with qual features for use in ML
 
 custom_data <- cbind(qual_data,prin_comp$x[,1:2])
-str(custom_data)
+
+### now let's try to predict stuff
+
+
 
 ### Train/test split
 
-library(caret)
+
 data3 <- createDataPartition(data2$readmitted, p = .8, list = FALSE, times = 1)
 train <- data2[data3, ]
 test  <- data2[-data3, ]
